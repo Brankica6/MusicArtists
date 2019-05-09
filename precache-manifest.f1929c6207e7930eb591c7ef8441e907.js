9674ec18cd919b6c3796b4db92f4b244cb790313
Brankica <brankicadjordjevic0@gmail.com>
2019-05-09 19:41:27 +0200

    Updates

---
+++ precache-manifest.f1929c6207e7930eb591c7ef8441e907.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a9a51111b1884857733",
+    "url": "/MusicArtists/static/js/runtime~main.5137e83e.js"
+  },
+  {
+    "revision": "41b4e81b33408d30f834",
+    "url": "/MusicArtists/static/js/main.434dcfb2.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/MusicArtists/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "41b4e81b33408d30f834",
+    "url": "/MusicArtists/static/css/main.66af015e.chunk.css"
+  },
+  {
+    "revision": "2521676bb86f4f760bbdca1dce75b2d9",
+    "url": "/MusicArtists/index.html"
+  }
+];
